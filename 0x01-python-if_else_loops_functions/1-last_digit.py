430545ac3594c373f022d290004e58d8a35918c6
MomanyiJ <momanyij04@gmail.com>
2022-06-15 03:29:11 -0700

    correction on requiremtn error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f"The number is {number}")
+
 if number < 0:
     last_digit = number % -10
     print(f"Last digit of {number} is {last_digit} ", end='')
