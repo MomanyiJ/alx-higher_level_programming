a8d3739a5c2a1da4034bc55ca09764a6b58733ec
MomanyiJ <momanyij04@gmail.com>
2022-06-15 13:40:42 -0700

    Corrected error on output

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 9):
-    for j in range(1, 10):
+    for j in range(1 + i, 10):
         if i == 8 and j == 9:
             print("{}{}".format(i, j))
         else:
