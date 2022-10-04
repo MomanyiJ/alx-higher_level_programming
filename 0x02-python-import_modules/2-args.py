46824d5ed192106af7027fe0395e406e2e8f38a0
MomanyiJ <momanyij04@gmail.com>
2022-10-04 05:36:50 -0700

    correction on pep8 error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    n = len(argv) -1
+    n = len(argv) - 1
     if n == 0:
         print("0 arguments.")
     elif n == 1:
