086e4898e0636ec8b5b5a083d8a6dfb98a7eb953
MomanyiJ <momanyij04@gmail.com>
2022-10-13 06:03:34 -0700

    function prinignting matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
+    text = ""
+    for line in matrix:
+        if not line:
+            text += "\n"
+            continue
+        text += "{:d}".format(linbe[0])
+        for i in range(1, len(line)):
+            text += " {:d}".format(line[i])
+        text += "\n"
+    print(text, end="")
