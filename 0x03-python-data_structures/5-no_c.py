096d8174abf86d7bd62ff1fcb1e4e910a1762c76
MomanyiJ <momanyij04@gmail.com>
2022-10-13 05:55:45 -0700

    typo error

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for c in my_string:
-        if c != 'c' and c != 'c':
+        if c != 'c' and c != 'C':
             new_string += c
     return new_string
