80e4287cbcfa74ce217c965bc2e8276eaaaa5c16
MomanyiJ <momanyij04@gmail.com>
2022-10-13 06:09:33 -0700

    correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,7 +7,7 @@ def print_matrix_integer(matrix=[[]]):
         if not line:
             text += "\n"
             continue
-        text += "{:d}".format(linbe[0])
+        text += "{:d}".format(line[0])
         for i in range(1, len(line)):
             text += " {:d}".format(line[i])
         text += "\n"
