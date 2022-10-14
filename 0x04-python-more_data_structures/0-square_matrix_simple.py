db6a59ddbe50e049dd02cadd5414eaf5d6875753
MomanyiJ <momanyij04@gmail.com>
2022-10-14 06:05:49 -0700

    punctuation error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [list(map((lambda x: x * x), num) for num in matrix]
+    return [list(map((lambda x: x * x)), ele) for ele in matrix]
