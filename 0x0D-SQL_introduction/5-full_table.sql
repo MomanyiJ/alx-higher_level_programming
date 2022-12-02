3b208bb7bcc51e7a4885fa0312387be3245abafc
MomanyiJ <momanyij04@gmail.com>
2022-12-01 17:52:19 -0800

    script printing full table description

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,7 @@
+-- Write a script that prints description of the table first_table in the hbtn_0c_0 DB 
+-- do not use DESCRIBE or EXPLAIN
+-- the DB name will be passed as an argument of the mysql command
+
+SHOW CREATE TABLE first_table;
+
+
