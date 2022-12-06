d2c43f1c9065e1dd962d69eb014cbbdc9d52a63b
MomanyiJ <momanyij04@gmail.com>
2022-12-06 04:20:45 -0800

    correction

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- script displaying average temp in farenheit by city ordered by temp descending
 
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avr_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
