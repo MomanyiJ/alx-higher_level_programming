6bda1157c5ab9cc5edf6ed48ae774eb281d08994
MomanyiJ <momanyij04@gmail.com>
2022-12-06 00:01:44 -0800

    script lisitng all rows

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- script lisitng all rows of table first_table from DB hbtn_0c_0 in MySQL server
+-- All fields should be printed
+-- The DB name will be passed as an argument of the mysql command
+
+SELECT * FROM first_table;
