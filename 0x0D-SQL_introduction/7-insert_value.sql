19fbbcb76e99f2532d2a102014c21e4ab9479050
MomanyiJ <momanyij04@gmail.com>
2022-12-06 00:13:52 -0800

    script inserting new row to table

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- script inserting a new row n the table first_table in DB hbtn_0c_0 in my MySQL server
+-- new row id = 89, name = Best School
+INSERT INTO 'first_table' ('id', 'name') VALUES(89, 'Best School');
