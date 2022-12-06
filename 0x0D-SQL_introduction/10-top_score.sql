d2ba46de0d13922d809432abc485cc90c244b10e
MomanyiJ <momanyij04@gmail.com>
2022-12-06 02:25:19 -0800

    script listing from highest to lowest from records in second_table

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- Script listing all records of table second_table of the DB hbtn_0c_0 in my MySQL server
+-- results should display both score and name in that order
+--records should be ordered by score(top first)
+-- the db name will be passed as an argument of the mysql command
+
+SELECT score, name FROM second_table ORDER BY score DESC;
