cb4eaed6ec1d30c9cd195b4b6efe88e6895cc959
MomanyiJ <momanyij04@gmail.com>
2022-12-06 03:12:43 -0800

    script computing average score of second_table records and assigning them to average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- script computing average score of all records in the table second_table
+-- result should be named average
+-- the DB name will be passed as an arg of the mysql command
+
+SELECT AVG(score) AS average FROM second_table;
