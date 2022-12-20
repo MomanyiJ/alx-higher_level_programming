63ba6ca767b9abfe1467f619418ddb460e1ac708
MomanyiJ <momanyij04@gmail.com>
2022-12-19 19:32:30 -0800

    correction on semi colons

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const myArguments = process.argv.length
+const myArguments = process.argv.length;
 if (myArguments === 2) {
-  console.log('No argument')
+  console.log('No argument');
 } else if (myArguments === 3) {
-  console.log('Argument found')
+  console.log('Argument found');
 } else {
-  console.log('Arguments found')
+  console.log('Arguments found');
 }
