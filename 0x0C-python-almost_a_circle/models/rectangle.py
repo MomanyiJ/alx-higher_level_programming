6e4b30def9278260ee85573f56a24d3d8a1dfae8
MomanyiJ <momanyij04@gmail.com>
2023-02-13 01:17:03 -0800

    added public method that returns value of the rectangle instance

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -70,3 +70,7 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError('y must be >= 0')
         self.__y = value
+
+    def area(self):
+        ''' returns the area of the rectangle '''
+        return (self.__width * self.height)
