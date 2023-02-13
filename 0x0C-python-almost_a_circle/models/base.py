6739b90311d047978fb5953f5c7b9dd98103df63
MomanyiJ <momanyij04@gmail.com>
2023-02-13 05:25:59 -0800

    adding json file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,4 +27,23 @@ class Base:
         if list_dictionaries is None or list_dictionaries == '[]':
             return "[]"
         else:
-            return json.dumps(list_dictionaries
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        ''' writes the JSON string representation of lists_objs to a file '''       filename = cls.__name__ + '.json'
+        dics = []
+        if lists_objs is None:
+            retun []
+        for lists in lists_objs:
+            dics.append(Lists.to_dictionary())
+
+        with open(filename, mode='w'0 as file:
+                file.write(Base.to_json_string(dics))
+
+    def from_json_string(json_string):
+        ''' from json to dic '''
+        if json_string is None or json_string == '[]':
+            return '[]'
+        return json.loads(json_string)
+
