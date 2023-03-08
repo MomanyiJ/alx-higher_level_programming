bdd8573e38f183bfde2dab2b4ac781dad7c8a012
MomanyiJ <momanyij04@gmail.com>
2023-03-08 01:22:35 -0800

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Class defining square """
+
+
+class Square:
+    """ Square class """
+    def __init__(self, size):
+        """ initializes the data """
+        self.__size = size
+
+
