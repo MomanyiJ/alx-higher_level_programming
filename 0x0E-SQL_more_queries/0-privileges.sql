57e40e82dd6a858ab4b86c602ee4163c637fdfbc
MomanyiJ <momanyij04@gmail.com>
2023-03-17 01:57:38 -0700

    corrections on error in comments

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
---list all privileges of the MYSQL users user_0d_1 and user_0d_2 on your server in localhost
-SHOW GRANTS for 'user_0d_1'@'localhost';
-SHOW GRANTS for 'user_0d_2'@'localhost';
+-- list all privileges of the MYSQL users user_0d_1 and user_0d_2 on your server in localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
