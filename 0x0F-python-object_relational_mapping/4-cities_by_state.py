18e52c2e1956fd760cc36bf28c814a9ff1ced273
MomanyiJ <momanyij04@gmail.com>
2023-04-17 23:15:29 +0300

    script listing all cities in the database

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" scrip listing cities from the database """
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connection(host="localhost", user=sys.argv[1],
+                            passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor
+    # s denotes variable named script
+    s = f"SELECT cities.id, cities.name, states.name {'FROM cities'}{
+            'LEFT OUTER JOIN states ON cities.state_id=states.id'}
+{'ORDER BY cities.id ASC}"
+    cur.execute(s)
+    data = cur.fetchall()
+    for row in data:
+        print(row)
+
+    cur.close()
+    database.close()
