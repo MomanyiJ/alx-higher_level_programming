36b8be0fe1134f145696279a34c9279849d56e1e
MomanyiJ <momanyij04@gmail.com>
2023-04-17 14:32:24 +0300

    added Binary to enforce strict selection of arg

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3],  port=3306)
     cur = db.cursor()
     # s is variable name for selecting state instruction
-    s = 'SELECT * FROM states WHERE name = "{}" ORDER by id ASC'
+    s = 'SELECT * FROM states WHERE BINARY name = "{}" ORDER by id ASC'
     cur.execute(s.format(sys.argv[4]))
     data = cur.fetchall()
     for state in data:
