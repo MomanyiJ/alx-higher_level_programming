dd693d31af97a1de6d321369fafa704298e23f74
MomanyiJ <momanyij04@gmail.com>
2023-04-17 14:14:47 +0300

    docstring error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+""" script taking an argument displaying all values in states table """
 
 import MySQLdb
 import sys
 
-""" script taking an argument displaying all values in states table """
 if __name__ =="__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
