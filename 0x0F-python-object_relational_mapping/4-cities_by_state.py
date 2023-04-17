c53a67ca0c3eca9888cebcf1a7872d37ae4a1fe2
MomanyiJ <momanyij04@gmail.com>
2023-04-17 23:24:56 +0300

    correction

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connection(host="localhost", user=sys.argv[1],
                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor
+    cur = db.cursor()
     # s denotes variable named script
     s = f"SELECT cities.id, cities.name, states.name {'FROM cities'}{
             'LEFT OUTER JOIN states ON cities.state_id=states.id'}
