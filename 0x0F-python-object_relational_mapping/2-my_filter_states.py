e09c5f7e2aed1f5ce78e3ad3eaa4e2ba2a124b2d
MomanyiJ <momanyij04@gmail.com>
2023-04-17 13:53:20 +0300

    script taking in arg and displaying values where name matches arg

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+""" script taking an argument displaying all values in states table """
+if __name__ =="__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    #s is variable name for selecting state instruction
+    s = f'SELECT * FROM states WHERE name = "{sys.argv[4]}" ORDER by id ASC'
+    cur.execute(s)
+    data = cur.fetchall()
+    for state in data:
+        print(state)
+
+    cur.close()
+    db.close()
+
+
