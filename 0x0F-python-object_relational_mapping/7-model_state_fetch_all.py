3af89887b9ef1f7b14a98cbf2a3c24b0c408a2e4
MomanyiJ <momanyij04@gmail.com>
2023-04-18 22:20:46 +0300

    script lisiting all states from the hbtn_0e_6_usa DB

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    # assigning variables
+    username, password, database = argv[1], argv[2], argv[3]
+    # connect to DB
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                            .format(username, password, database),
+                            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # session creation
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    # query all objects fromState and sorted by id in ascending order
+    states = session.query(State).order_by(State.id).all()
+
+    #prints the results
+
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
