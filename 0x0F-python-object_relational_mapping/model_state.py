1626537e39077319e5e7835d2f3db80685686786
MomanyiJ <momanyij04@gmail.com>
2023-04-18 17:25:06 +0300

    correction on brackets in integer that werent to be there

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,6 +12,6 @@ Base = declarative_base()
 class State(Base):
     """ State table """
     __tablename__ = 'states'
-    id = Column(Integer(), primary_key=True,
+    id = Column(Integer, primary_key=True,
                 nullable=False, unique=True, autoincrement=True)
     user = Column(String(128), nullable=False)
