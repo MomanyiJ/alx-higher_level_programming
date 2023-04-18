963c552f9a800669b00849d36b88a139b6aef4a6
MomanyiJ <momanyij04@gmail.com>
2023-04-18 17:15:09 +0300

    correction on pycodestyle...Remember to check it before pushing to github always

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,10 +5,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import String, Integer, Column
 from sqlalchemy import create_engine
 
-Base=declarative_base()
+Base = declarative_base()
 
-#Definig the class
+
+# Definig the class
 class State(Base):
-    __tablename__='states'
-    id=Column(Integer(),primary_key=True,nullable=False,unique=True, autoincrement=True)
-    user=Column(String(128),nullable=False)
+    __tablename__ = 'states'
+    id = Column(Integer(), primary_key=True,
+                nullable=False, unique=True, autoincrement=True)
+    user = Column(String(128), nullable=False)
