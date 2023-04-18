f68fe791d0375c1918c25bf90e99604ad240339d
MomanyiJ <momanyij04@gmail.com>
2023-04-18 10:42:54 +0300

    correction on indentation errors

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,14 +4,15 @@
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
     db = MySQLdb.connection(host="localhost", user=sys.argv[1],
                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     # s denotes variable named script
-    s = f"SELECT cities.id, cities.name, states.name {'FROM cities'}{
-            'LEFT OUTER JOIN states ON cities.state_id=states.id'}
-        {'ORDER BY cities.id ASC}"
+    s = f"SELECT cities.id, cities.name, states.name {'FROM cities'}/
+    {'LEFT OUTER JOIN states ON cities.state_id=states.id'}/
+    {'ORDER BY cities.id ASC}"
     cur.execute(s)
     data = cur.fetchall()
     for row in data:
