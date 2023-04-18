c8b6133ae822c216cf7da91e07aadd9baa014832
MomanyiJ <momanyij04@gmail.com>
2023-04-18 17:05:11 +0300

    file containing State class defiiniton for Base class

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" using SQL alchemy """
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import String, Integer, Column
+from sqlalchemy import create_engine
+
+Base=declarative_base()
+
+#Definig the class
+class State(Base):
+    __tablename__='states'
+    id=Column(Integer(),primary_key=True,nullable=False,unique=True, autoincrement=True)
+    user=Column(String(128),nullable=False)
