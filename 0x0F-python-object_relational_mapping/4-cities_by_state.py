8a81d0e42fa762a2dda45ebbfab546e82146abdb
MomanyiJ <momanyij04@gmail.com>
2023-04-18 11:06:23 +0300

    correction on pycodestyle error

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                            passwd=sys.argv[2], db=sys.argv[3], port=3306)
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     # s denotes variable named script
     s = "SELECT cities.id, cities.name, states.name FROM cities \
