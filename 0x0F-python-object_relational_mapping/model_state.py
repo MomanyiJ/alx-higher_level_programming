c62807827984324abb7646c464a8d60a5b857785
MomanyiJ <momanyij04@gmail.com>
2023-04-19 11:28:07 +0300

    error in naming field in sql that was causing fails in other question replaced user with name on DB

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-""" using SQL alchemy """
 
+from sqlalchemy import String,Integer,Column
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import String, Integer, Column
-from sqlalchemy import create_engine
+
 
 Base = declarative_base()
 
 
-# Definig the class
 class State(Base):
-    """ State table """
-    __tablename__ = 'states'
+    """
+    Represents a State in US.
 
+    Attributes include
+    id(int): unique state identifier
+    name(str): Name of stae
+    """
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+# connect to MySQL server
+#engine = create_engine('mysql+mysqlconnector://root:password@localhost:3306/mydatabase')
+
+#Base.metadat.create_all(engine)
