06ef60ca4c4929f0a24b9c6782ae68c5e51f96d7
MomanyiJ <momanyij04@gmail.com>
2023-06-06 13:37:16 +0300

    1

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    
+    total_sum = 0
+    #we use the set function cause it takes unique integers
+    for num in set(my_list):
+        total_sum += num
+            
+    return total_sum
