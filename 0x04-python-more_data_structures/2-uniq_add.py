666c40dc69a4a7a41dbf2aa551717598dbc1a983
MomanyiJ <momanyij04@gmail.com>
2023-06-06 13:45:44 +0300

    correction on pycodestyle error

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    
     total_sum = 0
-    #we use the set function cause it takes unique integers
+    # we use the set function cause it takes unique integers
     for num in set(my_list):
         total_sum += num
-            
+
     return total_sum
