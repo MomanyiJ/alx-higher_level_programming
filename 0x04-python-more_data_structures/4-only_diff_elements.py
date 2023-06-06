1efdfb49f97990e2f04ab4e58184bafed85117dd
MomanyiJ <momanyij04@gmail.com>
2023-06-06 14:48:37 +0300

    correction on pycodestyle error

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    # first part checks elements not in set 2 from set one and vice versa for 2nd part
-    # | combines the two sets to create a new set containgi the unique elements from both sets
+    # first part checks elements not in set 2 from
+    # set one and vice versa for 2nd part
+    # | combines the two sets to create a
+    # new set containgi the unique elements from both sets
     present_one = (set_1 - set_2) | (set_2 - set_1)
     return present_one
