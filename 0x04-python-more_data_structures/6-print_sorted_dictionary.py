7e00e60aa8e45e2d65524ccee8a8aaebcd638228
MomanyiJ <momanyij04@gmail.com>
2023-06-07 15:48:11 +0300

    correction on spaCES THAT CAUSE CHECKER NOT TO WORK

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     keys_sorted = sorted(a_dictionary.keys())
 
     for key in keys_sorted:
-        print(key, ":", a_dictionary[key])
+        print(f'{key}: {a_dictionary[key]}')
