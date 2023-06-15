2b42620f2da822adb7b3f618745e8f8bde3f8e51
MomanyiJ <momanyij04@gmail.com>
2023-06-15 16:42:19 +0300

    stupendous

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const threeIn1 = ["C is fun", "Python is cool", "Javascript is amazing"];
-for(let i=0; i < threeIn1.length; i++){
+for (let i=0; i < threeIn1.length; i++) {
 	console.log(threeIn1[i]);
 }
