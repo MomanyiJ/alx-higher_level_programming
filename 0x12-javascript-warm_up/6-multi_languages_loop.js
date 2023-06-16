08a99cf78f9b1df9ae5b38ab4ede769e7be03886
MomanyiJ <momanyij04@gmail.com>
2023-06-16 12:16:12 +0300

    solve typo error capital S in JavaScript output

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const threeIn1 = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const threeIn1 = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < threeIn1.length; i++) {
   console.log(threeIn1[i]);
 }
