ab80098a94941b5d53891db14f11aeb51ce89fe4
MomanyiJ <momanyij04@gmail.com>
2023-08-11 12:53:29 +0300

    solved identation error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ if number < 0:
     last_digit = number % -10
     print(f"Last digit of {number} is {last_digit} ", end='')
 else:
-     last_digit = number % 10
-     print(f"Last digit of {number} is {last_digit} ", end='')
+    last_digit = number % 10
+    print(f"Last digit of {number} is {last_digit} ", end='')
 if last_digit > 5:
     print(f"and is greater than 5")
 elif last_digit == 0:
