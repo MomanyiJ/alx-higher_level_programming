f95ce93fa21a61807d618e96a95e2cb122693d57
MomanyiJ <momanyij04@gmail.com>
2023-10-24 20:34:55 +0300

    correction on error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,6 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
+            result = 0
         except TypeError:
             print("Wrong type")
             result = 0
