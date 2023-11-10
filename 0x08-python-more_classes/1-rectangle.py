cd1eecab879df9acd230a2b8285701c05890b565
Momanyi <momanyij04@gmail.com>
2023-11-10 12:37:50 +0300

    corrected pycodestyle error, and missing be

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         """int: The width of rectangle."""
@@ -41,5 +41,5 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("height must >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
