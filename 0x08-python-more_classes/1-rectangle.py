e2750b022b39801da332a2279defe4999526776d
Momanyi <momanyij04@gmail.com>
2023-11-10 12:29:25 +0300

    typo errors and signs eg + instead of 0

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -26,9 +26,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
-            raise TypeError("Width must be an integer")
+            raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("Width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -41,5 +41,5 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("height must >+ 0")
+            raise ValueError("height must >= 0")
         self.__height = value
