dfa5807768355eda3e0b0f96214e67f48cf40be5
Momanyi <momanyij04@gmail.com>
2023-11-16 13:13:33 +0300

    correction on typo

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
-            raise VaueError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
